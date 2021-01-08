363438c64ae92de5cd5f6bcee85d284e2785ae61
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2021-01-08 14:47:34 +0630

    Before Composables

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLHzOchY_PEPHmJ7yjHL4wOPtej9WVdZw",
+  authDomain: "muso-ninjas-vue3.firebaseapp.com",
+  projectId: "muso-ninjas-vue3",
+  storageBucket: "muso-ninjas-vue3.appspot.com",
+  messagingSenderId: "1058420018511",
+  appId: "1:1058420018511:web:abd06f29e9b7a03fb0893a"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirebase = firebase.firestore()
+const projectAuth = firestore.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirebase, projectAuth, timestamp }
