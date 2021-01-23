2c323cdc6906367ec52e82489a15b58ef62d9802
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2021-01-24 00:36:36 +0630

    Nav Update & Route Guard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="home">
-    <p>Homepage</p>
+    <div v-if="error" class="error">Could not fetch the data.</div>
+    <div v-if="documents">
+      <ListView :playlists="documents" />
+    </div>
   </div>
 </template>
 
 <script>
+import ListView from '../components/ListView.vue'
+import getCollection from '../composables/getCollection'
 
 export default {
-  name: 'Home'
+  components: { ListView },
+  name: 'Home',
+  setup() {
+    const { error, documents } = getCollection('playlists')
+
+
+    return { error, documents }
+  }
 }
 </script>
