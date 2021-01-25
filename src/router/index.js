c746b71095ddbf427f1a1f26177bb6028df6be85
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2021-01-25 23:56:38 +0630

    Playlist Details Component

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import Login from '../views/auth/Login.vue'
 import Signup from '../views/auth/Signup.vue'
 import CreatePlaylist from '../views/playlists/CreatePlaylist.vue'
+import PlaylistDetails from '../views/playlists/PlaylistDetails.vue'
 
 // route guard
 import { projectAuth } from '../firebase/config'
@@ -39,6 +40,13 @@ const routes = [
     component: CreatePlaylist,
     beforeEnter: requireAuth
   },
+  {
+    path: '/playlists/:id',
+    name: 'PlaylistDetails',
+    component: PlaylistDetails,
+    beforeEnter: requireAuth,
+    props: true
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/playlists/PlaylistDetails.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>Playlist ID - {{id}}</h2>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+
+  setup(props) {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
