2ea3d92b22ced5f2ed0495b45884c1f404c682b8
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2021-02-17 19:33:00 +0630

    Getting single document

---
+++ src/composables/getDocument.js
@@ -0,0 +1,33 @@
+import { ref, watchEffect } from 'vue'
+import { projectFirestore } from '../firebase/config'
+
+const getDocument = (collection, id) => {
+
+  const documents = ref(null)
+  const error = ref(null)
+
+  // register the firestore collection reference
+  let documentRef = projectFirestore.collection(collection).doc(id)
+
+  const unsub = documentRef.onSnapshot(doc => {
+    if(doc.data()) {
+      document.value = {...doc.data(), id: doc.id }
+      error.value = null
+    } else {
+      error.value = 'That document does not exist.'
+    }
+    
+  }, err => {
+    console.log(err.message)
+    
+    error.value = 'could not fetch the document'
+  })
+
+  watchEffect((onInvalidate) => {
+    onInvalidate(() => unsub());
+  });
+
+  return { error, documents }
+}
+
+export default getDocument
